3d2a811c908e5df303a266ee6c11a437d24bb3e5
Vishnu Sai <70326555+vishnusai399@users.noreply.github.com>
2026-07-27 06:46:25 +0000

    first commit

---
+++ Day1/notes.js
@@ -0,0 +1,7 @@
+process.stdout.write("Let's learn Node.js");
+
+
+//nodejs is a run time env for js
+//JS works only in browser because of v8 engine but if u want to execute js code outside of browser u cant so to run js code outside broser u need nodejs 
+//v8 engine embedded with c++ which forms nodejs run time env
+//Node.js = V8 Engine + libuv + Node APIs (fs, http, stream, etc.), which allows JavaScript to run outside the browser.
